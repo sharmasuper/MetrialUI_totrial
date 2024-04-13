dc5fa7ccc23f6c0ccdcb2325c687daa50b7b1617
sharmasuper <mohit6375sharma@gmail.com>
2024-04-14 03:30:10 +0530

    typography

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+import './App.css'
+import Test from './Test'
+
+function App() {
+  
+
+  return (
+    <>
+     
+      <p>Hello</p>
+      <Test/>
+     
+    </>
+  )
+}
+
+export default App
---
+++ src/Test.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Typography from '@mui/material/Typography';
+function Test() {
+  return (
+   <div>
+    <Typography variant="h5" sx={{color:"red",border:"2px solid black",borderRadius:'10px'}} >Hello world type</Typography>
+   </div>
+  );
+}
+
+export default Test;
