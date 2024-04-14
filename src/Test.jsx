170825878d40ff249e3506e28b85096a22536be6
sharmasuper <mohit6375sharma@gmail.com>
2024-04-14 17:04:06 +0530

    checkboxuse

--- src/Test.jsx
+++ src/Test.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import TextField from '@mui/material/TextField'
-import { Typography } from '@mui/material';
+import { Checkbox, FormControlLabel, FormGroup, Typography } from '@mui/material';
 function Test() {
    const [inputs,setInputs] = useState({
     name:"",
     email:"",
-    passworld:""
+    passworld:"",
+    subscribe: false
    })
  
    const handleChange = (e) =>{
@@ -21,15 +22,23 @@ function Test() {
  e.preventDefault();
  console.log(inputs)
 
-
  }
   return (
    <div>
- <form onSubmit={handleSubmit}>
+ <form onSubmit={handleSubmit} style={{display:"flex",flexDirection:"column"}}>
 <TextField type={'text'} value={inputs.name} name='name' onChange={handleChange} sx={{margin:3}} id="outlined-basic" label="Outlined" variant="outlined" />
       <TextField type={"email"}  name='email' value={inputs.email} onChange={handleChange} sx={{margin:3}} id="filled-basic" label="Filled" variant="filled" />
       <TextField type={'password'} name='passworld' value={inputs.passworld} onChange={handleChange} sx={{margin:3}} id="standard-basic" label="Standard" variant="standard" />
      
+<FormGroup>
+  <FormControlLabel control={<Checkbox onChange={()=>setInputs((pre)=>({
+    ...pre,subscribe:!inputs.subscribe
+  }))} />} label="subscribe the news latter" />  
+</FormGroup>
+
+
+
+
      <button type='submit'>Submit</button>
       </form> 
    </div>
