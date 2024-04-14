771ecf5dcbe2a433bc445e59b3140ed57f57cfbe
sharmasuper <mohit6375sharma@gmail.com>
2024-04-14 14:24:24 +0530

    formHandling

--- src/Test.jsx
+++ src/Test.jsx
@@ -2,15 +2,36 @@ import React, { useState } from 'react';
 import TextField from '@mui/material/TextField'
 import { Typography } from '@mui/material';
 function Test() {
-    const [Name,setname] = useState("")
+   const [inputs,setInputs] = useState({
+    name:"",
+    email:"",
+    passworld:""
+   })
+ 
+   const handleChange = (e) =>{
+    setInputs((predata)=>({
+        ...predata,
+        [e.target.name] : e.target.value
 
+    }))
+   
+   }
+
+ const handleSubmit = (e) =>{
+ e.preventDefault();
+ console.log(inputs)
+
+
+ }
   return (
    <div>
-
-<TextField type={'text'} value={Name} onChange={(e)=>setname(e.target.value)} sx={{margin:3}} id="outlined-basic" label="Outlined" variant="outlined" />
-      <TextField type={"email"}  sx={{margin:3}} id="filled-basic" label="Filled" variant="filled" />
-      <TextField type={'password'} sx={{margin:3}} id="standard-basic" label="Standard" variant="standard" />
-      <Typography>{Name}</Typography>
+ <form onSubmit={handleSubmit}>
+<TextField type={'text'} value={inputs.name} name='name' onChange={handleChange} sx={{margin:3}} id="outlined-basic" label="Outlined" variant="outlined" />
+      <TextField type={"email"}  name='email' value={inputs.email} onChange={handleChange} sx={{margin:3}} id="filled-basic" label="Filled" variant="filled" />
+      <TextField type={'password'} name='passworld' value={inputs.passworld} onChange={handleChange} sx={{margin:3}} id="standard-basic" label="Standard" variant="standard" />
+     
+     <button type='submit'>Submit</button>
+      </form> 
    </div>
   );
 }
