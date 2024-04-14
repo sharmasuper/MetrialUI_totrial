b84337ad1e3c385937e4216813517a6a418e6bb5
sharmasuper <mohit6375sharma@gmail.com>
2024-04-14 12:21:25 +0530

    textfield

--- src/Test.jsx
+++ src/Test.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
-import Button from '@mui/material/Button'
+import React, { useState } from 'react';
+import TextField from '@mui/material/TextField'
+import { Typography } from '@mui/material';
 function Test() {
+    const [Name,setname] = useState("")
+
   return (
    <div>
-   <Button onClick={()=>alert("button Clicked frist")} color="success" sx={{margin:3}} size="small"  variant="text">frist</Button>
-   <Button onClick={()=>alert("button Clicked second")} color="error" sx={{margin:3}} size="medium" variant="contained">Second</Button>
-   <Button onClick={()=>alert("button Clicked third")} color="secondary" sx={{margin:3}} size="large" variant="">third</Button>
+
+<TextField type={'text'} value={Name} onChange={(e)=>setname(e.target.value)} sx={{margin:3}} id="outlined-basic" label="Outlined" variant="outlined" />
+      <TextField type={"email"}  sx={{margin:3}} id="filled-basic" label="Filled" variant="filled" />
+      <TextField type={'password'} sx={{margin:3}} id="standard-basic" label="Standard" variant="standard" />
+      <Typography>{Name}</Typography>
    </div>
   );
 }
