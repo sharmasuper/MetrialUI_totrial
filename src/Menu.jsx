ed9203c8e49030098d21a88d76a21103bffebe68
sharmasuper <mohit6375sharma@gmail.com>
2024-04-15 21:53:38 +0530

    modal

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,4 +1,4 @@
-import { AppBar, Button, Card, CardActions, CardContent, CardMedia, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Tab, Tabs, Toolbar, Typography } from "@mui/material";
+import { AppBar, Box, Button, Card, CardActions, CardContent, CardMedia, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Modal, Tab, Tabs, Toolbar, Typography } from "@mui/material";
 import { useState } from "react";
 
 function Menu() {
@@ -7,18 +7,12 @@ function Menu() {
    return (
   <div>
     <Button onClick={()=>setopen(true)}>Open Dialog</Button>
-     <Dialog hideBackdrop open={open} onClose={()=>setopen(false)}>
-        <DialogTitle>This is my Dialog</DialogTitle>
-        <DialogContent>
-            <DialogContentText>
-               A dialog is a popup window which can ask user simething like an infomation or conformation
-            </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-            <Button onClick={()=>setopen(false)}>cancel</Button>
-            <Button onClick={()=>setopen(false)}>agree</Button>
-        </DialogActions>
-     </Dialog>
+     <Modal hideBackdrop open={open} onClose={()=>setopen(false)}>
+       <Box position="absolute" top="50%" left="50%">
+        <Typography>It is a Modal</Typography>
+         <Button onClick={()=>setopen(false)}>Click Me</Button>
+       </Box>
+     </Modal>
     
   </div>
 
