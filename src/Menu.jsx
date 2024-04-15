f8e2f149202e18124ae350afe46a78a2907eb1ca
sharmasuper <mohit6375sharma@gmail.com>
2024-04-15 18:38:52 +0530

    ui tabs

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,31 +1,27 @@
+import { AppBar, Tab, Tabs, Toolbar } from "@mui/material";
 import { useState } from "react";
-import { Menu as MuiMenu, MenuItem, Button } from "@mui/material"; // Aliased import
 
 function Menu() {
-    const [anchorElm,setAnchorElm] = useState(null)
-    const [open,setopen] = useState(false);
+    const [value,setvalue] = useState(0)
 
-    const handleClose = () =>{
-        setAnchorElm(null);
-        setopen(false)
-    }
-    const handleClick = (e) =>{
-  setAnchorElm(e.currentTarget)
-  setopen(true)
-    }
+   return (
+  <div>
+    <AppBar>
+        <Toolbar>
+        <Tabs 
+    indicatorColor="secondary" textColor="secondary"
+    value={value} onChange={(e,val)=>setvalue(val)}>
+        <Tab label="frist"></Tab>
+        <Tab label="second"></Tab>
+        <Tab label="third"></Tab>
+    </Tabs>
+        </Toolbar>
+    </AppBar>
+    
+  </div>
 
-    return (
-        <div>
-            <Button varient="contained" onClick={handleClick}> open Menu</Button>
-            <Button varient="contained" onClick={handleClick}>open Menu again</Button>
-            <MuiMenu anchorEl={anchorElm} open={open} onClose={handleClose}>
-                <MenuItem onClick={handleClose}>Profile</MenuItem>
-                <MenuItem onClick={handleClose}>Balance</MenuItem>
-                <MenuItem onClick={handleClose}>Logout</MenuItem>
-            </MuiMenu>
-           
-        </div>
-    );
+
+   ) 
 }
 
 export default Menu;
