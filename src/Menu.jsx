0eaad48b0796826e7a1fc048d495c8983ea57aa3
sharmasuper <mohit6375sharma@gmail.com>
2024-04-15 19:06:33 +0530

    MetrialUi

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,4 +1,4 @@
-import { AppBar, Tab, Tabs, Toolbar } from "@mui/material";
+import { AppBar, Button, Card, CardActions, CardContent, CardMedia, Tab, Tabs, Toolbar, Typography } from "@mui/material";
 import { useState } from "react";
 
 function Menu() {
@@ -6,17 +6,26 @@ function Menu() {
 
    return (
   <div>
-    <AppBar>
-        <Toolbar>
-        <Tabs 
-    indicatorColor="secondary" textColor="secondary"
-    value={value} onChange={(e,val)=>setvalue(val)}>
-        <Tab label="frist"></Tab>
-        <Tab label="second"></Tab>
-        <Tab label="third"></Tab>
-    </Tabs>
-        </Toolbar>
-    </AppBar>
+     <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image="/static/images/cards/contemplative-reptile.jpg"
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Lizard
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
     
   </div>
 
