79f2927b151f24715ea719c318758b60fc8d46a1
sharmasuper <mohit6375sharma@gmail.com>
2024-04-16 23:16:40 +0530

    theme customize

--- src/Appbar.jsx
+++ src/Appbar.jsx
@@ -1,61 +1,17 @@
 
-import { Box } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
+import { Box, Typography } from '@mui/material';
 import { DataGrid } from '@mui/x-data-grid'
-const columns = [
-  { field: 'id', headerName: 'ID', width: 90 },
-  {
-    field: 'firstName',
-    headerName: 'First name',
-    width: 150,
-    editable: true,
-  },
-  {
-    field: 'lastName',
-    headerName: 'Last name',
-    width: 150,
-    editable: true,
-  },
-  {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 110,
-    editable: true,
-  },
-  {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (value, row) => `${row.firstName || ''} ${row.lastName || ''}`,
-  },
-];
-
-const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 14 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 31 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 31 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 11 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-];
-
-
-
-
 
 
 function Appbar() {
+  const theme = useTheme()
+   console.log(theme)
   return (
-      <Box height={400} witdh={800}>
-        <DataGrid onCellEditCommit={(params)=>console.log(params)} columns={columns} rows={rows}  checkboxSelection>
-
-        </DataGrid>
-      </Box>  
+      <Box>
+         <Typography color="primary">mssharma</Typography>
+         <h1>Mohit Sharma</h1>
+      </Box>
   );
 }
 
---
+++ src/Theme.jsx
@@ -0,0 +1,26 @@
+import { createTheme } from '@mui/material';
+
+
+ export const theme = createTheme({
+    palette : {
+        primary:{
+            main:"#F4005E",
+        }
+    },
+    
+breakpoints:{
+    values :{
+        sm:500
+    }
+
+},
+ 
+typography : {
+    h1:{
+        fontSize:"100px"
+    }
+}
+
+})
+
+
